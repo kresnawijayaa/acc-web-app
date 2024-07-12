1ebdfd92abd24a5eea3a49f54ecce9b6061dcdc4
kresna <kresna.wijaya@indomaret.co.id>
2024-07-12 20:12:33 +0700

    initial commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
-//app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const customerRoutes = require('./routes/customerRoutes');
 
@@ -10,7 +10,15 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Middleware untuk melayani file statis dari build React
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/customers', customerRoutes);
+
+// Menangani permintaan lain dan mengarahkan ke index.html dari build React
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 module.exports = app;
